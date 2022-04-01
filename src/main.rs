e8479e7262d5473fc4ab241a17a6e29b183b9507
Cole Poirier <colepoirier@gmail.com>
2022-03-31 20:26:35 -0700

    broken app construction ordering version

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins)
         .insert_resource(WindowDescriptor {
             title: "Test Title".to_string(),
             width: 1920.0,
             height: 1080.0,
             ..Default::default()
         })
-        .add_plugins(DefaultPlugins)
         .run();
 }
